7eff6bface851c3a3ee033b5e08cd10d5a4e8c13
Simon Madsen <simon@codespace.dk>
2020-06-02 12:27:11 +0200

    shadow bug

--- shadow.js
+++ shadow.js
@@ -2,17 +2,17 @@ const calcShadow = no => {
   switch (no) {
     case 1:
       return [
-        ['shadowOffset', '0'],
+        ['shadowOffset', { width: 0, height: 0}],
         ['shadowRadius', 0.75]
       ]
     case 2:
       return [
-        ['shadowOffset', '0 0.75'],
+        ['shadowOffset', { width: 0, height: 0.75}],
         ['shadowRadius', 1, 5]
       ]
     default:
       return [
-        ['shadowOffset', `0 ${no - 1}`],
+        ['shadowOffset', { width: 0, height: no - 1}],
         ['shadowRadius', no]
       ]
   }
